ad6dc69ab0c24bc060f7060232504d62591bc9b2
Guest User <Guest@epicodus-41.local>
2018-10-01 12:59:58 -0700

    add updated files

---
+++ Assets/Scripts/CloudGen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGen : MonoBehaviour
+{
+
+    public GameObject cloud;
+    public float width = 10;
+    public float height = 10;
+    public float cloudSize = 5;
+
+    void Start()
+    {
+        for (int x = 0; x < width;x++)
+        {
+            for (int y = 0; y < height;y++)
+            {
+                GameObject cloudCube = Instantiate(cloud, new Vector3(transform.position.x + x * cloudSize, transform.position.y, transform.position.z + y * cloudSize),Quaternion.identity);
+
+                cloudCube.name = "Cloud_" + x + "_" + y;
+                cloudCube.transform.SetParent(transform);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovementRadiusRenderer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ** IMPORTANT NOTES **
+// NEED TO CHANGE XRADIUS AND YRADIUS TO USE MOVEMENT DISTANCE ON STAT SHEET FOR EACH CHARACTER.
+
+
+// ** BUGS **
+// - Doesn't resize even when entering new radius in script. Seems to only take variables at start up.
+
+[RequireComponent(typeof(LineRenderer))]
+public class MovementRadiusRenderer : MonoBehaviour {
+
+	//number of lines in circle.
+	public int segments = 50;
+
+	//xradius of circle.
+	public float xradius = 8;
+
+	//yradius of circle.
+	public float yradius = 8;
+
+	//Variable to hold the line renderer component.
+	LineRenderer line;
+
+	void Start ()
+	{
+		//Grabs the line renderer component from game object.
+		line = gameObject.GetComponent<LineRenderer>();
+		
+		line.positionCount = (segments + 1);
+		line.useWorldSpace = false;
+		CreatePoints ();
+	}
+
+	void CreatePoints ()
+	{
+		float x;
+		// float y;
+		float z;
+
+		float angle = 20f;
+
+		for (int i = 0; i < (segments + 1); i++)
+		{
+			x = Mathf.Sin (Mathf.Deg2Rad * angle) * xradius;
+			z = Mathf.Cos (Mathf.Deg2Rad * angle) * yradius;
+
+			line.SetPosition (i,new Vector3(x,0,z) );
+
+			angle += (360f / segments);
+		}
+	}
+}
