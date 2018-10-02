71935c2f63cc18b020e2cb507a53648cce8ef026
SkyeN <skye@dames.es>
2018-10-01 23:30:15 -0700

    connect fog of war to PC

---
+++ Assets/Scripts/Guy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guy : MonoBehaviour {
+
+    public CloudGen cloud;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I'm the guy");
+        Debug.Log(transform.GetInstanceID());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cloud.mapObj = this.gameObject;
+	}
+}
