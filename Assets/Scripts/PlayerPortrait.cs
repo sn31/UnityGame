c61b649bb0b299404863975c72f7dbe9c3879ed4
Brian Nelson <nelsonsbrian@gmail.com>
2018-10-01 17:57:50 -0700

    add framework for UI Portraits for players

---
+++ Assets/Scripts/PlayerPortrait.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPortrait : MonoBehaviour 
+{
+	private GameObject[] pcList;
+	private int index;
+
+	void Start () 
+	{
+		if (pcList == null)
+		{
+			UpdatePCList();
+		}		
+	}
+	
+
+	void Update () 
+	{
+		UpdatePlayerHealth();
+	}
+
+
+	void UpdatePCList()
+	{
+		pcList = GameObject.FindGameObjectsWithTag("PlayableCharacter");
+	}
+
+	void UpdatePortraitImg()
+	{
+		//   for(int i = 0; i < 4; i++)
+		//   {
+			
+		//   }
+
+		//  set img to prefab image based on index of photo
+
+	}
+
+	void UpdatePlayerHealth()
+	{
+
+	}
+
+}
